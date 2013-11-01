615ebfcd7c04c766ba17d7ec86fddafeae36920c
Kenneth-Chen <kennchen@gmail.com>
2013-11-01 11:04:01 -0400

    add referrer tracking to contacts

---
+++ assets/global-8a1087564f2da694ded9414b3345bdd0.js
@@ -0,0 +1,1 @@
+(function(){$(document).ready(function(){$(".company-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100});$(".consultant-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100});return $("input#referrer").val(document.referrer)})}).call(this);
--- assets/global-af57404ee0d8487eed50b51afd03792d.js
+++
@@ -1,1 +0,0 @@
-(function(){$(document).ready(function(){$(".company-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100});return $(".consultant-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100})})}).call(this);
