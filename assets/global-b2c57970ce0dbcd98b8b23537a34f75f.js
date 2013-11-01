4383c63042fa434dba549976e8c0d8490a7b5aec
Kenneth-Chen <kennchen@gmail.com>
2013-11-01 13:59:24 -0400

    remove ref from string

--- assets/global-b2c57970ce0dbcd98b8b23537a34f75f.js
+++
@@ -1,1 +0,0 @@
-(function(){$(document).ready(function(){$(".company-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100});$(".consultant-form").colorbox({inline:true,innerWidth:"700px",fixed:true,speed:250,fadeOut:100});return $("input#referrer").val(window.location.search.replace("?",""))})}).call(this);
