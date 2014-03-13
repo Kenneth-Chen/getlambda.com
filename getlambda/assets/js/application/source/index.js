9c9660de29ca53a4bf737f197dccb3622c5f60e5
Kenneth Chen <kennchen@gmail.com>
2014-03-13 17:40:39 -0400

    make site better

---
+++ getlambda/assets/js/application/source/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    /* Fix navigation. */
+    $('#navigation').css(".opacity","0");
+    $('#navigation').fixedonlater({
+        speedDown: 250,
+        speedUp: 100
+    });
+});
+
