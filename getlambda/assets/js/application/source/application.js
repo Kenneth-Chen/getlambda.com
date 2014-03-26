e9ff0920d34e911a4261b41b8d749f46466f6da0
Kenneth Chen <kennchen@gmail.com>
2014-03-26 18:09:09 -0400

    turn off parallax for welcome page; add background image for welcome page behind video; remove extra unused images

--- getlambda/assets/js/application/source/application.js
+++ getlambda/assets/js/application/source/application.js
@@ -63,7 +63,7 @@ $(document).ready(function(){
     */
     if(pageWidth > 980){
         /* Dont user paralax for tablet and mobile devices. */
-        $('#page-welcome').parallax("0%", 0.2);
+        // $('#page-welcome').parallax("0%", 0.2);
         $('.page-talent').parallax("0%", 0.07);
         $('.page-about').parallax("0%", 0.1);
         $('.page-contact').parallax("0%", 0.1);
